56c2920c8150e71d137a1435fa8371b52cbc4fe3
rethishkr <rethishkumar.p@gmail.com>
2025-06-23 06:32:00 +0400

    added 3 new ml model codes

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,14 @@
 
 
 print("adding all the model code related to fraud detection")
+def model1():
+    print("Adding code to ML model 1")
+    # Add model code here
+
+def model2():
+    print("Adding code to ML model 2")
+    # Add model code here
+
+def model3():
+    print("Adding code to ML model 3")
+    # Add model code here
